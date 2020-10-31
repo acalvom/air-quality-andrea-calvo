a90e5113e38121ca3522d958d709dd2f932ca8ae
acalvom <andreac.3108@gmail.com>
2020-10-31 20:13:58 +0100

    Improve and reformat code

--- app/src/main/java/es/upm/miw/airquality/AQIParameter.java
+++ app/src/main/java/es/upm/miw/airquality/AQIParameter.java
@@ -6,6 +6,23 @@ import static es.upm.miw.airquality.MainActivity.LOG_TAG;
 
 public class AQIParameter {
 
+    final static int CO_GOOD       = 400;
+    final static int CO_MODERATE   = 800;
+    final static int NO2_GOOD      = 50;
+    final static int NO2_MODERATE  = 100;
+    final static int PM25_GOOD     = 15;
+    final static int PM25_MODERATE = 30;
+    final static int PM10_GOOD     = 25;
+    final static int PM10_MODERATE = 50;
+    final static int O3_GOOD       = 65;
+    final static int O3_MODERATE   = 120;
+    final static int SO2_GOOD      = 50;
+    final static int SO2_MODERATE  = 120;
+
+    final static int AQI_GOOD      = 0;
+    final static int AQI_MODERATE  = 1;
+    final static int AQI_UNHEALTHY = 2;
+
     private String parameterName;
     private Double value;
 
@@ -37,63 +54,62 @@ public class AQIParameter {
 
         }else if(parameterName.equals("co")){
             Log.i(LOG_TAG, "Parameter is: Carbon Monoxide");
-            if(value < 400){
-                AQILevel = 0;
-            }else if (value >= 400 && value < 800){
-                AQILevel = 1;
+            if(value < CO_GOOD){
+                AQILevel = AQI_GOOD;
+            }else if (value >= CO_GOOD && value < CO_MODERATE){
+                AQILevel = AQI_MODERATE;
             }else{
-                AQILevel = 2;
+                AQILevel = AQI_UNHEALTHY;
             }
 
         }else if(parameterName.equals("no2")){
             Log.i(LOG_TAG, "Parameter is: Nitrogen Dioxide");
-            if(value < 50){
-                AQILevel = 0;
-            }else if (value >= 50 && value < 100){
-                AQILevel = 1;
+            if(value < NO2_GOOD){
+                AQILevel = AQI_GOOD;
+            }else if (value >= NO2_GOOD && value < NO2_MODERATE){
+                AQILevel = AQI_MODERATE;
             }else{
-                AQILevel = 2;
+                AQILevel = AQI_UNHEALTHY;
             }
 
         }else if(parameterName.equals("pm25")){
             Log.i(LOG_TAG, "Parameter is: Suspended particulates smaller than 2.5 μm");
-            if(value < 15){
-                AQILevel = 0;
-            }else if (value >= 15 && value < 30){
-                AQILevel = 1;
+            if(value < PM25_GOOD){
+                AQILevel = AQI_GOOD;
+            }else if (value >= PM25_GOOD && value < PM25_MODERATE){
+                AQILevel = AQI_MODERATE;
             }else{
-                AQILevel = 2;
+                AQILevel = AQI_UNHEALTHY;
             }
 
         }else if(parameterName.equals("pm10")){
             Log.i(LOG_TAG, "Parameter is: Suspended particulates smaller than 10 μm");
-            if(value < 25){
-                AQILevel = 0;
-
-            }else if (value >= 25 && value < 50){
-                AQILevel = 1;
+            if(value < PM10_GOOD){
+                AQILevel = AQI_GOOD;
+            }else if (value >= PM10_GOOD && value < PM10_MODERATE){
+                AQILevel = AQI_MODERATE;
             }else{
-                AQILevel = 2;
+                AQILevel = AQI_UNHEALTHY;
             }
 
         }else if(parameterName.equals("o3")){
             Log.i(LOG_TAG, "Parameter is: Ozone");
-            if(value < 65){
-                AQILevel = 0;
-            }else if (value >= 65 && value < 120){
-                AQILevel = 1;
+            if(value < O3_GOOD){
+                AQILevel = AQI_GOOD;
+            }else if (value >= O3_GOOD && value < O3_MODERATE){
+                AQILevel = AQI_MODERATE;
             }else{
-                AQILevel = 2;
+                AQILevel = AQI_UNHEALTHY;
             }
 
         }else if(parameterName.equals("so2")){
             Log.i(LOG_TAG, "Parameter is: Sulfur Dioxide");
-            if(value < 50){
-                AQILevel = 0;
-            }else if (value >= 50 && value < 120){
-                AQILevel = 1;
+            if(value < SO2_GOOD){
+                AQILevel = AQI_GOOD;
+            }else if (value >= SO2_GOOD && value < SO2_MODERATE){
+                AQILevel = AQI_MODERATE;
             }else{
-                AQILevel = 2;
+                AQILevel = AQI_UNHEALTHY;
             }
 
         }
